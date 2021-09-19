6af74ea17d0a403446e1d688dffedf2ffbd87c2e
gauri <gas@carecom.com>
2021-09-19 21:30:30 +0200

    Main class to test scenarios

---
+++ src/main/java/com/application/Application.java
@@ -0,0 +1,63 @@
+package main.java.com.application;
+
+import main.java.com.engine.PromotionEngine;
+import main.java.com.model.order.Order;
+import main.java.com.model.product.Product;
+import main.java.com.service.product.ProductService;
+import main.java.com.service.promotion.PromotionService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+    public static PromotionService promotionService = new PromotionService();
+    public static ProductService productService = new ProductService();
+
+    public static void main(String[] args){
+        Order order1 = new Order();
+        List<Product> products = new ArrayList<>();
+
+        products.add(new Product("A"));
+        products.add(new Product("B"));
+        products.add(new Product("C"));
+        order1.setProducts(products);
+
+
+        PromotionEngine eng = new PromotionEngine(promotionService, productService);
+        double amount = eng.calculateAmount(order1);
+        System.out.println(" Order1 : " + amount);
+        Order order2 = new Order();
+
+        products.add(new Product("A"));
+        products.add(new Product("A"));
+        products.add(new Product("A"));
+        products.add(new Product("A"));
+        products.add(new Product("B"));
+        products.add(new Product("B"));
+        order1.setProducts(products);
+        amount = eng.calculateAmount(order1);
+        System.out.println(" Order2 : " + amount);
+
+        products.add(new Product("D"));
+        order1.setProducts(products);
+        amount = eng.calculateAmount(order1);
+        System.out.println(" Order3 : " + amount);
+
+        products.add(new Product("C"));
+        products.add(new Product("D"));
+        order1.setProducts(products);
+        amount = eng.calculateAmount(order1);
+        System.out.println(" Order4 : " + amount);
+
+        products.add(new Product("C"));
+        order1.setProducts(products);
+        amount = eng.calculateAmount(order1);
+        System.out.println(" Order5 : " + amount);
+
+        products.add(new Product("D"));
+        products.add(new Product("D"));
+        order1.setProducts(products);
+        amount = eng.calculateAmount(order1);
+        System.out.println(" Order6 : " + amount);
+    }
+}
