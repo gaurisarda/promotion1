af46437f5c204eae2d71eed990bd99863198c64e
gauri <gas@carecom.com>
2021-09-19 20:02:36 +0200

    Basic structure for PromotionEngine

---
+++ src/main/java/com/engine/PromotionEngine.java
@@ -0,0 +1,16 @@
+package main.java.com.engine;
+
+import main.java.com.model.order.Order;
+
+public class PromotionEngine {
+
+
+    public double calculateAmount(Order order){
+
+        double totalAmount = 0;
+
+        return totalAmount;
+    }
+
+
+}
